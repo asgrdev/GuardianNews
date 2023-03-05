e6a3e93e320bd47c7d384da96474a3fd416bb874
asgrdev <asghari.a68@gmail.com>
2023-03-05 17:45:24 +0330

    add animated  splash screen

--- app/src/main/java/org/asghari/guardiannews/presentation/composableviews/SplashScreen.kt
+++ app/src/main/java/org/asghari/guardiannews/presentation/composableviews/SplashScreen.kt
@@ -1,18 +1,19 @@
 package org.asghari.guardiannews.presentation.composableviews
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import kotlinx.coroutines.delay
@@ -20,25 +21,55 @@ import org.asghari.guardiannews.other.ScreensRoute.HomePage
 
 @Composable
 fun SplashScreen(navController: NavController) {
+    var isVisible by remember { mutableStateOf(false) }
+    var timeToShowNews by remember { mutableStateOf(false) }
+    val trasition = updateTransition(targetState = isVisible, label = null)
+    var newsplaceString  by remember { mutableStateOf("         ")}
+    val color by trasition.animateColor(
+        transitionSpec = { tween(3000) },
+        label = "changeColor",
+        targetValueByState = {isVisible ->
+            if(!isVisible) Color(173, 236, 243, 240) else  Color(3, 140, 202, 255)
+        }
+    )
     Box(
         Modifier
             .fillMaxWidth()
             .fillMaxHeight()
-
     )
     {
         Column(modifier = Modifier.align(Alignment.Center)) {
+            Row {
+                AnimatedVisibility(visible = isVisible, enter = fadeIn(animationSpec = tween(2000)))
+                {
+                    Text(
+                        "The Guardian",
+                        style = TextStyle(
+                            fontWeight = FontWeight.Bold,
+                            color = color,
+                            fontSize = 31.sp,
+                            textAlign = TextAlign.Center
+                        )
+                    )
+                }
 
-            Text(
-                "The Guardian",
-                style = TextStyle(
-                    color = Color.Blue,
-                    fontSize = 25.sp,
-                    textAlign = TextAlign.Center
+                Text(
+                    newsplaceString,
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        color = Color(2, 60, 87, 255),
+                        fontSize = 31.sp,
+                        textAlign = TextAlign.Center
+                    )
                 )
-            )
+            }
             LaunchedEffect(true) {
-                delay(2000L)
+                delay(50L)
+                isVisible = true
+                delay(2800L)
+                timeToShowNews = true
+                newsplaceString = " News"
+                delay(600L)
                 navController.popBackStack()
                 navController.navigate(HomePage().route)
             }
